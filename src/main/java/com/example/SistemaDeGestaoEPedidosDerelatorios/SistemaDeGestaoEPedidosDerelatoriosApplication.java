40889638a57a657b4f59dde803a38f5c310b5d20
Dianaguedes19 <1241903@isep.ipp.pt>
2025-07-17 17:11:28 +0100

    Initial commit with the project structure

---
+++ src/main/java/com/example/SistemaDeGestaoEPedidosDerelatorios/SistemaDeGestaoEPedidosDerelatoriosApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaDeGestaoEPedidosDerelatorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoEPedidosDerelatoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoEPedidosDerelatoriosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SistemaDeGestaoEPedidosDerelatorios/SistemaDeGestaoEPedidosDerelatoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaDeGestaoEPedidosDerelatorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoEPedidosDerelatoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
