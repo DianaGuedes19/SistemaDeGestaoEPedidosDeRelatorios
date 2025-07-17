203f1c6ff1c5e3563dca3663b562e38aba248f7d
Dianaguedes19 <1241903@isep.ipp.pt>
2025-07-17 18:21:10 +0100

    Added new restrictions to Order entity. Ref #2

--- src/main/java/com/example/SistemaDeGestaoEPedidosDerelatorios/domain/Order.java
+++ src/main/java/com/example/SistemaDeGestaoEPedidosDerelatorios/domain/Order.java
@@ -1,11 +1,12 @@
 package com.example.SistemaDeGestaoEPedidosDerelatorios.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import jdk.vm.ci.meta.Local;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 
@@ -21,23 +22,35 @@ public class Order {
 
     @Getter
     @Setter
+    @NotNull (message = "Client name cannot be null")
+    @NotBlank (message = "Client name cannot be in blank")
     private String clientName;
 
     @Getter
     @Setter
+    @NotNull (message = "Client email cannot be null")
+    @NotBlank (message = "Client email cannot be in blank")
+    @Email (message = "Email format is wrong, should contain @ and domain")
     private String clientEmail;
 
     @Getter
     @Setter
+    @NotNull (message = "LocalDate cannot be null")
+    @NotBlank (message = "LocalDate cannot be in blank")
     private LocalDate creationDate;
 
     @Getter
     @Setter
+    @NotNull (message = "Status cannot be null")
+    @NotBlank (message = "Status cannot be in blank")
     @Enumerated(EnumType.STRING)
     private State status;
 
     @Getter
     @Setter
+    @NotNull (message = "Value cannot be null")
+    @NotBlank (message = "Value cannot be in blank")
+    @PositiveOrZero(message = "Value cannot be 0 or below")
     private double value;
 
 
