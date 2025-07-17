bcc72df56ecbd1128c494f89e83513037317366b
Dianaguedes19 <1241903@isep.ipp.pt>
2025-07-17 17:58:55 +0100

    Creation of order DTO. Ref #2

---
+++ src/main/java/com/example/SistemaDeGestaoEPedidosDerelatorios/DTO/orderDTO.java
@@ -0,0 +1,40 @@
+package com.example.SistemaDeGestaoEPedidosDerelatorios.DTO;
+
+import com.example.SistemaDeGestaoEPedidosDerelatorios.domain.State;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class orderDTO {
+
+    @Getter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String clientName;
+
+    @Getter
+    @Setter
+    private String clientEmail;
+
+    @Getter
+    @Setter
+    private LocalDate creationDate;
+
+    @Getter
+    @Setter
+    @Enumerated(EnumType.STRING)
+    private State status;
+
+    @Getter
+    @Setter
+    private double value;
+}
