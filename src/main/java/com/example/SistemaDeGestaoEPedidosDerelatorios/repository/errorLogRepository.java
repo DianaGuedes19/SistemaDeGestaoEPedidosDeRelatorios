e11f02282e7d55c8a932749a6347591ab3ee4557
Dianaguedes19 <1241903@isep.ipp.pt>
2025-07-18 18:38:27 +0100

    Implementing email sender in application.properties. Ref #7

--- src/main/java/com/example/SistemaDeGestaoEPedidosDerelatorios/repository/errorLogRepository.java
+++ src/main/java/com/example/SistemaDeGestaoEPedidosDerelatorios/repository/errorLogRepository.java
@@ -3,5 +3,10 @@ package com.example.SistemaDeGestaoEPedidosDerelatorios.repository;
 import com.example.SistemaDeGestaoEPedidosDerelatorios.domain.ErrorLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface errorLogRepository extends JpaRepository<ErrorLog,Long> {
+    List<ErrorLog> findByTimestampBetween(LocalDateTime from, LocalDateTime to);
+
 }
