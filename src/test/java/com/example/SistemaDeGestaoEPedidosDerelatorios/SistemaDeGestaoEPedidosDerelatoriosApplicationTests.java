90bb89327a227bf4baaa4ed00fcd86102cf4291e
Dianaguedes19 <1241903@isep.ipp.pt>
2025-07-18 13:24:55 +0100

    Updated application-mssql.yml and pom.xml Ref #4

--- src/test/java/com/example/SistemaDeGestaoEPedidosDerelatorios/SistemaDeGestaoEPedidosDerelatoriosApplicationTests.java
+++ src/test/java/com/example/SistemaDeGestaoEPedidosDerelatorios/SistemaDeGestaoEPedidosDerelatoriosApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.SistemaDeGestaoEPedidosDerelatorios;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "validation.url=http://localhost:8080")class SistemaDeGestaoEPedidosDerelatoriosApplicationTests {
+@SpringBootTest()class SistemaDeGestaoEPedidosDerelatoriosApplicationTests {
 
 	@Test
 	void contextLoads() {
